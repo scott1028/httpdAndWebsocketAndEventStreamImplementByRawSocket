a017597392709187958062ac5a1716adc42ba744
scott1028 <mic1028002@gmail.com>
2013-10-16 17:24:31 +0800

    new file:   socket_implement_httpd.py

---
+++ socket_implement_httpd.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+# 實作 HTTP Daemon
+
+import socket,re,sys
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+sock.bind(("", 9999))  
+sock.listen(10)
+
+while True:
+	con, address = sock.accept()
+	try:
+		# 因為要避免 con 斷線的時候 host send client 拋錯
+		print con, address
+		con.send('HTTP/1.1 200 OK\r\n')
+		con.send('Content-Type: text/html\r\n')
+		con.send('Content-Length: 10\r\n\r\n')
+		con.send('hello world')
+		data=con.recv(1024)
+		con.close()
+		print data
+	except:
+		pass
+
+sock.close()
